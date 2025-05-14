07cf5b132c65f284294198ddd773e8b01576c860
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-05-14 15:04:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Nadhif Safaraz Pranggana")
 st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
+
+col1, col2, col3 = st.columns(3)
+
+with col1
+  st.header("kucing")
+  st.image("https://static.streamlit.io/examples/cat.jpg")
